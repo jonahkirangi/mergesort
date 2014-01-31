369477ddf80f0ca773054a46639c50c7c3a4b1de
Jonah Kirangi <jonahkirangi@gmail.com>
2014-01-31 13:56:54 -0800

    Fixed array.size bug

--- mergesort.rb
+++ mergesort.rb
@@ -1,7 +1,7 @@
 class MergeSort
 
   def mergesort(array)
-    return array if array < 1
+    return array if array.size <= 1
     second_array = array.slice((array.size/2).round, array.size)
     left_sorted_array = mergesort(array)
     right_sorted_array = mergesort(second_array)
